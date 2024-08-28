f067b44a2e3295573271774ba273629982913abe
cyrusc2026 <cyrusc2026@student.cis.edu.hk>
2024-08-28 09:40:44 +0800

    Commit 1

---
+++ src/Delimiters.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Delimiters {
+    private final String openDel;
+    private final String closeDel;
+
+    public String getOpenDel() {
+        return openDel;
+    }
+
+    public String getCloseDel() {
+        return closeDel;
+    }
+
+    public Delimiters(String open, String close){
+        this.openDel = open;
+        this.closeDel = close;
+    }
+
+    public ArrayList<String> getDelimitersList(String[] token) {
+        ArrayList<String> delimiters = new ArrayList<>();
+        for (String s : token) {
+            if (s.equals(openDel)) {
+                delimiters.add(openDel);
+            }
+            if (s.equals(closeDel)) {
+                delimiters.add(closeDel);
+            }
+        }
+        return delimiters;
+    }
+    public boolean isBalanced(ArrayList delimiters) {
+        int hasOpen = 0;
+        for (int i =0; i < delimiters.size(); i++){
+            if (delimiters.get(i).equals(openDel)){
+                hasOpen++;
+            }
+            else if (delimiters.get(i).equals(closeDel)){
+                if (hasOpen <= 0) return false;
+                else {
+                    hasOpen--;
+                }
+            }
+        }
+        return (hasOpen==0);
+    }
+
+
+}
